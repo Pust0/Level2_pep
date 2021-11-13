0702fe6a7b00214599192703ccb98a6a0509c563
Varun <47032093+Pust0@users.noreply.github.com>
2021-11-13 10:25:57 +0530

    Update nqueensVariations.java

--- recursion/12-11-2021/nqueensVariations.java
+++ recursion/12-11-2021/nqueensVariations.java
@@ -125,7 +125,8 @@ class nqueensVariations{
 		count+=nqueens_perm(board, tq, row, col+1, ans, rows, cols, diag, adiag);
 		return count;
 	}
-
+	
+	//leetcode 52-N Queens 2
 	public static int nqueens_comb_op(boolean[][] board, int tq, int row, int col, boolean[] rows, boolean[] cols, boolean[] diag, boolean[] adiag){
 		
 		if(tq==0){
@@ -188,4 +189,58 @@ class nqueensVariations{
 //		 nqueens_comb(new boolean[n][m], tq, 0, 0, "", rows, cols, diag, adiag);
 		// System.out.println(nqueens_perm(new boolean[n][m], tq, 0, 0, "", rows, cols, diag, adiag));
 	}
+	
+	
+	//leetcode 51 - N Queens 1
+	class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        boolean[][] grid = new boolean[n][n];
+        List<List<String>> result = new ArrayList<>();
+        explore(grid, result, n, 0);
+        return result;
+    }
+    
+    void explore(boolean[][] grid, List<List<String>> result, int n, int row){
+        if(n == row){
+            result.add(getGridAsListOfStrings(grid));
+            return;
+        }
+        
+        for(int i=0; i < n; i++){
+            if(canPlaceQueen(grid, row, i, n)){
+                grid[row][i] = true;
+                explore(grid, result, n, row+1);
+                grid[row][i] = false;
+            }
+        }
+    }
+    
+    boolean canPlaceQueen(boolean[][] grid, int row, int col, int n){
+        for(int i=row-1; i >=0; i--)
+            if(grid[i][col]) return false;
+        
+        for(int i=row-1, j=col-1; i >=0 && j >= 0; i--, j--)
+            if(grid[i][j]) return false;        
+        
+        for(int i=row-1, j=col+1; i >=0 && j < n; i--, j++)
+            if(grid[i][j]) return false;
+        
+        return true;
+    }
+    
+    List<String> getGridAsListOfStrings(boolean[][] grid){
+        List<String> ans = new ArrayList<>();
+        for(int i=0; i < grid.length; i++){
+            StringBuilder currRow = new StringBuilder();
+            for(int j=0; j < grid.length; j++){
+                if(grid[i][j]) 
+                    currRow.append("Q");
+                else
+                    currRow.append(".");
+            }
+            ans.add(currRow.toString());
+        }
+        return ans;
+    }
+}
 }
